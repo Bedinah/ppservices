c8919d83c3a746c8e8c07043a42c796364821667
Bedinah <bernabazubagira@gmail.com>
2024-03-02 10:02:24 +0200

    default landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,13 @@
 import LandingPage from "@/pages/pages/landing";
-<<<<<<< HEAD
-import Image from "next/image";
-=======
 import Navigation from "@/pages/pages/navigation";
 import Services from "@/pages/pages/service";
->>>>>>> 76b646d (landing page)
 
 export default function Home() {
   return (
     <main>
-<<<<<<< HEAD
-      <LandingPage/>
-=======
-      <Navigation/>
-      <LandingPage/>
-      <Services/>
->>>>>>> 76b646d (landing page)
+      <Navigation />
+      <LandingPage />
+      <Services />
     </main>
   );
 }
