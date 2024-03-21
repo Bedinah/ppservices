f26088705620bb1ca85f8d729e77e8bbb1f2dc1a
Bedinah <bernabazubagira@gmail.com>
2024-03-21 12:01:30 +0200

    nitial commit

--- pages/pages/service.tsx
+++ pages/pages/service.tsx
@@ -29,7 +29,7 @@ const Services = () => {
     <div className=" flex justify-center  min-h-screen w-full py-4 gap-[18rem] ">
           <div className="w-[30%]">
             <div className="font-bold text-[4rem] leading-[1.1] mt-[6rem]">
-              services I <br/> offer
+              About Me
             </div>
             <div className="flex gap-3 mt-8">
               <hr className="bg-[#333333] w-20 h-[2px] mt-3" />
@@ -43,6 +43,7 @@ const Services = () => {
           </div>
         </div>
         <div className="w-[25%] mt-[6rem]" >
+        <h1 className="font-semibold text-base">Services I offer</h1>
           {services?.map((data: any, index: any) => (
             <>
               <Divider/>
